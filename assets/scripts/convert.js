72e0b0e66e89a066584456bc926e345d51f366e4
pauld0051 <pauld0051@gmail.com>
2020-11-30 14:24:18 +0000

    Add separate tables for °C to °F and vice versa

--- assets/scripts/convert.js
+++ assets/scripts/convert.js
@@ -1,12 +1,15 @@
 // Convert temperatures
 
-document.getElementById("submit").addEventListener("click", calculate_c);
+document.getElementById("submit_C").addEventListener("click", calculate_c);
 function calculate_c () {
     let T_in_C = parseFloat(document.getElementById("deg_celsius").value);
 
     let T_convert_C = (T_in_C * 9/5) + 32;
     document.getElementById("deg_celsius_conv").innerHTML = T_convert_C.toFixed(2) + " °F";
+}
 
+document.getElementById("submit_F").addEventListener("click", calculate_f);
+function calculate_f () {
     let T_in_F = parseFloat(document.getElementById("deg_fahrenheit").value);
 
     let T_convert_F = (T_in_F - 32) * 5/9;
